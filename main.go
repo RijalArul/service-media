dc7fff79fb56c9f7aa02938379585d61cb003877
Bri_Arul <fakhrul.konsultan@work.bri.co.id>
2022-11-30 11:03:20 +0700

    Setup Database

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+	"service-media/databases"
+
+	"github.com/joho/godotenv"
+)
+
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func main() {
+	databases.StartDB()
+}
